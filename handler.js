e94b38bea3ed7077a2c38d8b0b8075a756345f5e
Edgar Delgadillo <edgar.delgadillo@bigcheese.com>
2025-07-02 16:13:53 -0500

    git-secrets

--- handler.js
+++ handler.js
@@ -1,6 +1,8 @@
 'use strict';
 
 module.exports.hello = async (event) => {
+  const AWS_SECRET_ACCESS_KEY = "AKIAEXAMPLECLAVE123";
+  console.log("AWS_SECRET_ACCESS_KEY", AWS_SECRET_ACCESS_KEY);
   return {
     statusCode: 200,
     body: JSON.stringify(
