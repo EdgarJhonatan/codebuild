a3703c2a29261708a8f708f31764dcac4480587f
Edgar Delgadillo <edgar.delgadillo@bigcheese.com>
2025-07-02 19:45:57 -0500

    Validate push 003

--- handler.js
+++ handler.js
@@ -1,8 +1,6 @@
 'use strict';
 
 module.exports.hello = async (event) => {
-  const AWS_SECRET_ACCESS_KEY = "AKIA1234567890ABCDEF";
-  console.log("AWS_SECRET_ACCESS_KEY", AWS_SECRET_ACCESS_KEY);
   return {
     statusCode: 200,
     body: JSON.stringify(
