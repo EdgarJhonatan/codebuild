dc2ce38a4db34654e71f186462c04b23565e8b2b
Edgar Delgadillo <edgar.delgadillo@bigcheese.com>
2025-10-13 10:43:38 -0500

    test vulnerabilidad 3

--- vulnerable.js
+++ vulnerable.js
@@ -2,18 +2,18 @@
 const express = require('express');
 const app = express();
 
-// Vulnerabilidad: SQL INJECTION 💥
+// Vulnerabilidad: SQL INJECTION
 app.get('/user', (req, res) => {
   const userId = req.query.id;
-  const sql = "SELECT * FROM users WHERE id = " + userId; // ❌ inseguro
+  const sql = "SELECT * FROM users WHERE id = " + userId;
   res.send("Consulta ejecutada: " + sql);
 });
 
-// Vulnerabilidad: XSS 💥
+// Vulnerabilidad: XSS
 app.get('/xss', (req, res) => {
   const name = req.query.name;
-  res.send(`<h1>${name}</h1>`); // ❌ contenido sin sanitizar
+  res.send(`<h1>${name}</h1>`);
 });
 
 // Vulnerabilidad: Hardcoded secret
-const password = "SuperSecret123"; // ❌ debe denunciar
+const password = "SuperSecret123";
